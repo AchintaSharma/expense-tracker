1a623c51c764fe3dca555ecfd66322996e7d4e6a
Achinta Sharma <23achinta@gmail.com>
2023-08-28 11:35:54 +0530

    create expense chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,6 +23,7 @@ const Expenses = (props) => {
         onChangeFilter={filterChangeHandler}
       />
       <ExpensesList items={filteredExpenses} />
+      <ExpenseChart expenses={filteredExpenses} />
     </Card>
   );
 };
